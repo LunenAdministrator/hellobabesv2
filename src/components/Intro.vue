51696a954420c7f78cb0c45c8c7d60a3dc6269b7
Lunen Developer <71112248+LunenAdministrator@users.noreply.github.com>
2021-06-11 09:43:40 +0800

    version1.8

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -14,22 +14,25 @@
 				</v-card>
 			</v-col>
 			<v-col cols="12" md="6">
-				<v-card flat tile class="justify-center">
+				<v-card flat tile class="d-flex justify-center">
 					<img
 						src="https://i.pinimg.com/originals/b7/4d/23/b74d23f9a0b1afcde1151f47433e508f.png"
 						alt=""
-						min-width="100"
+						max-height="150"
+						max-width="250"
 						class="pa-2"
 					/>
 					<img
 						src="https://lh3.googleusercontent.com/PsiIC8ZM_-21soCElBkkKqbohXd7lyL7TenHue5INueXqqtafPgZq0MtHBsYqBv-tfIteWtfDQxG2-HBpPYUpRFiy6BkQiqkeySYXl2UepStr4GfWe4CXKNrC6zkpMSGtqhYn1Fu3A=w2400"
 						alt=""
-						min-width="100"
+						max-height="150"
+						max-width="250"
 						class="pa-2"
 					/>
 					<img
 						src="https://i.pinimg.com/originals/88/ad/fc/88adfc25878cc177ae00eedcf389c4cd.png"
-						min-width="100"
+						max-height="150"
+						max-width="250"
 						alt=""
 						class="pa-2"
 					/>
